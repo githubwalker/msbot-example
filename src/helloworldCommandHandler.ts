5842dd24a885e9e78d9792af7c606f8af6637d7b
Andrei Lebedev <alebedeff@yandex.ru>
2025-02-24 18:58:30 +0100

    amend caption

--- src/helloworldCommandHandler.ts
+++ src/helloworldCommandHandler.ts
@@ -18,7 +18,7 @@ export class HelloWorldCommandHandler implements TeamsFxBotCommandHandler {
 
     const cardJson = new ACData.Template(helloWorldCard).expand({
       $root: {
-        title: "Your Hello World App is Running",
+        title: "Your Hello World App is Running !!!",
         body: "Congratulations! Your Hello World App is running. Open the documentation below to learn more about how to build applications with the Teams Toolkit.",
       },
     });
