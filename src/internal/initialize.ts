b3528bb4eb0e5225a5b3ef4d5e57b01403d54477
Andrei Lebedev <alebedeff@yandex.ru>
2025-02-24 15:43:07 +0100

    azure pwds and redefine start

--- src/internal/initialize.ts
+++ src/internal/initialize.ts
@@ -7,6 +7,13 @@ import config from "./config";
 // Create the command bot and register the command handlers for your app.
 // You can also use the commandApp.command.registerCommands to register other commands
 // if you don't want to register all of them in the constructor
+console.log(`config is : ${JSON.stringify(config)}`)
+
+// https://learn.microsoft.com/en-us/azure/key-vault/secrets/quick-create-node?tabs=azure-cli%2Clinux&pivots=programming-language-javascript
+// https://github.com/Azure-Samples/azure-sdk-for-js-keyvault-secret-get-set-managedid/blob/master/v4/index.js
+// https://learn.microsoft.com/en-us/azure/key-vault/general/rbac-guide?tabs=azure-portal
+// https://www.youtube.com/watch?v=h987jU2X0MA
+
 export const commandApp = new ConversationBot({
   // The bot id and password to create CloudAdapter.
   // See https://aka.ms/about-bot-adapter to learn more about adapters.
